ad9cb75f5f8ce00033e9279b83161c497219e44a
dedelg000 <daniel.leme000@gmail.com>
2025-06-26 20:15:05 -0300

    Simulacao santander sem custom

--- src/scripts/Santander/testSantanderAuth.ts
+++ src/scripts/Santander/testSantanderAuth.ts
@@ -1,6 +1,5 @@
-import { decryptJasypt, encryptJasypt } from 'Utils/crypto'
-
-const original = '41525947'
-const encrypted = encryptJasypt(original)
+import { decryptJasypt, encryptAes, encryptJasypt } from 'Utils/crypto'
+const original = 'dyjGYAMS0qKnzPZt/JmMKoGOaGGoNzSB'
+const encrypted = encryptAes(original)
 console.log('Encrypted:', encrypted)
 console.log('Decrypted:', decryptJasypt('83sbAcPbUs2b935cguFkqcx36la2iQMI')) // Deve retornar "meu segredo"
